e29300c0529311a95f2750abb7eac4ed1c11d70a
chykron <o.perehuda@gmail.com>
2024-09-27 11:08:04 +0300

    added for character loop

--- test_task_.py
+++ test_task_.py
@@ -1,6 +1,6 @@
 import re
 
-file = open('/Users/oleh.perehuda/Documents/03 SCRAPY/projects/scrapers_class/pdf_processing/ab_raw.txt', 'r', encoding="UTF-8")
+file = open('/Users/oleh.perehuda/Documents/03 SCRAPY/projects/scrapers_class/pdf_processing/monocl_class/ab_raw.txt', 'r', encoding="UTF-8")
 
 text = []
 lines = []
@@ -23,7 +23,6 @@ for line in file:
 
 posters.append(text)
 
-
 for poster in posters[1:]:
     abstract_flag = False
     header = []
@@ -36,24 +35,38 @@ for poster in posters[1:]:
         else:
             header.append(element)
 
-    # check_empty_lists(poster[0], header, poster_text)
-    
+    # check_empty_lists(poster[0], header, poster_text)    
     affiliation_flag = False
     title_and_authors = []
-    affiliations = []
+    affiliations_raw = []
     
     if len(header) == 4:
-        title_and_authors == header[1:3]
-        affiliations == header[-1]
+        title_and_authors = header[1:3]
+        title = header[1]
+        authors = header[2].split(",")
+        affiliations = header[-1]        
     else:        
         for item in header:
             if re.match(r"^1(?=[A-Z]*[a-z]*)", item):
                 affiliation_flag = True
             if affiliation_flag:
-                affiliations.append(item)
+                affiliations_raw.append(item)
             else:
                 title_and_authors.append(item)
-    
-    check_empty_lists(header[0], title_and_authors, affiliations)
+                
+        affiliations = re.split(r",(?=\d)", "".join(affiliations_raw))
+        title = []
+        authors = []
+        authors_flag = False
+        for character in title_and_authors:
+            if not character.isupper():
+                authors_flag = True
+            if authors_flag:
+                authors.append(character)
+            else:
+                title.append(character)
 
+        check_empty_lists(header[0], title, authors)
+    
+    
 file.close()
